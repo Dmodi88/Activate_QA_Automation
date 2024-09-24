837f85e62392420b048998a44e07f5d9550bfde6
sandeepshivnani <sandeep.shivnani@gmail.com>
2024-09-24 16:22:48 -0400

    sc report smoke

--- cypressPOM/ui/cypress/PageObjects/homePage.js
+++ cypressPOM/ui/cypress/PageObjects/homePage.js
@@ -10,33 +10,9 @@ class HomePage
     clearbtn = "//button[normalize-space()='Clear']"
     selectAll = "(//span[normalize-space()='Select All'])[1]"
     dataExporter_Link ="//span[text()='Data Exporter']"
-
-    // profdropdownbtn = "div[role='button']"
     
-    // adminProfile = "li[aria-label='Admin Role'] div[aria-label='Admin Role']"
-    // greenprofile = "div[aria-label='Evgeni Grin']"
-    // retailerProfile = "div[aria-label='Retailer profile - without data export']"
-    // gregProfile = "div[aria-label='Gregorian Calendar Profile']"
-    // profileApplybtn = "(//button[normalize-space()='Apply'])[1]"
-    // home  = "//span[normalize-space()='Home']"
-    // msdHover = '//span[text()="Market Share ($)"]'
-    // msuHover = '//span[text()="Market Share Unit"]'
-    // msdHovertxt = "//*[text()='Percentage of sales for the retailer out of the total NIQ Channel, inc all participating Retailers in the market']"
-    // msuHovertxt = "//*[text()='Percentage of unit sales for the retailer out of the total NIQ Channel, inc all participating Retailers in the market']"
-    // homepagetitle = "//div[text()='Home Page']"
-    // kpiCardbox = "//div[@class='HomeCardsPanelComp__ChartsWrapperStyle-sc-r1tj2i-3 iZORex']/div"
-    // filterCardbox = "(//div[@class='HomeCardsPanelComp__FiltersWrapperStyle-sc-r1tj2i-2 ljDxkG'])[1]/div"
-    // category = "(//span[contains(text(),'Category')])[1]"
-    // brand = "(//span[contains(text(),'Brand')])[1]"
-    // geo = "(//span[contains(text(),'GEO')])[1]"
-    // bostonNorth = "//p[normalize-space()='BOSTON - NORTH']"
-    // comparativeMarket = "(//h3[normalize-space()='CMxAOC'])[1]"
-    // cmxaoc = "(//h3[normalize-space()='CMxAOC'])[2]"
-    // comparativePeriod = "(//h3[normalize-space()='Previous'])[1]"
-    // totalCompperiod = "//ul[@role='listbox']/li"
-    // yoy = "//h3[normalize-space()='Year Over Year']"
-    // loader = "//span[@role='progressbar']//*[name()='svg']"
 
+    
 
 
     openDataExporter()
@@ -44,6 +20,7 @@ class HomePage
         cy.xpath(this.dataExporter_Link).click()
     }
 
+    
     clickkpiDropdown1()
     {
         cy.xpath(this.kpiDropdown1).click()
@@ -155,138 +132,6 @@ class HomePage
     {
         cy.xpath(this.selectAll).should("be.visible")
     }
-
-
-
-
-    // verifyLogin()
-    // {
-    //     cy.get(this.profdropdownbtn)
-    // }
-
-    // clickDropdownbtn()
-    // {
-    //     cy.get(this.profdropdownbtn).click()
-    // }
-
-    // selectAdmnprofile()
-    // {
-    //     cy.get(this.adminProfile).click()
-    // }
-
-    // selectGreenprofile()
-    // {
-    //     cy.get(this.greenprofile).click()
-    // }
-
-    // selectRetailerprofile()
-    // {
-    //     cy.get(this.retailerProfile).click()
-    // }
-
-    // selectGregprofile()
-    // {
-    //     cy.get(this.gregProfile).click() 
-    // }
-
-    // clickApply()
-    // {
-    //     cy.xpath(this.profileApplybtn).click()
-    // }
-
-    // verifyLandingpage()
-    // {
-    //     cy.xpath(this.home).wait(5000) 
-    // }
-
-    // marketSharedollhover()
-    // {
-    //     cy.xpath(this.msdHover).trigger('mouseover').wait(2000)
-    // }
-
-    // marketShareunithover()
-    // {
-    //     cy.xpath(this.msuHover).trigger('mouseover').wait(3000)       
-    // }
-
-    // verifyMSDtext()
-    // {
-    //     cy.xpath(this.msdHovertxt).wait(3000)
-    // }
-
-    // verifyMSUtext()
-    // {
-    //     cy.xpath(this.msuHovertxt).wait(3000)
-    // }
-
-    // verifyHomepagetitle()
-    // {
-    //     cy.xpath(this.homepagetitle).click({force: true})
-    // }
-
-    // verifyTotalkpicards()
-    // {
-    //     cy.xpath(this.kpiCardbox).should('have.length',5)
-    // }
-
-    // verifyTotalfilters()
-    // {
-    //     cy.xpath(this.filterCardbox).should('have.length',5)
-    // }
-
-    // verifyCategory()
-    // {
-    //     cy.xpath(this.category).click({force: true}).wait(3000)
-    // }
-
-    // verifyBrand()
-    // {
-    //     cy.xpath(this.brand).click({force: true}).wait(3000)
-    // }
-
-    // verifyGEO()
-    // {
-    //     cy.xpath(this.geo).click({force: true}).wait(12000) 
-    // }
-
-    // // waitforGeoloader()
-    // // {
-    // //     cy.xpath(this.geo).click({force: true}).wait(12000) 
-    // // }
-
-
-    // selectGeoBostan()
-    // {
-    //     cy.xpath(this.bostonNorth).contains("BOSTON - NORTH").click().wait(3000) 
-    // }
-
-    // verifyComparativeMarket()
-    // {
-    //     cy.xpath(this.comparativeMarket).click({force: true}).wait(3000)
-    // }
-
-    // selectCMAxOC()
-    // {
-    //     cy.xpath(this.cmxaoc).click({force: true}).wait(2000)
-    // }
-
-    // selectCompareperiod()
-    // {
-    //     cy.xpath(this.comparativePeriod).click({force: true})
-    // }
-
-    // verifytotalCompareperiod()
-    // {
-    //     cy.xpath(this.totalCompperiod).should('have.length', 2)     
-    // }
-
-    // selectYOY()
-    // {
-    //     cy.xpath(this.yoy).click()
-    // }
-
-   
-
 
 }
 export default HomePage;
---
+++ cypressPOM/ui/cypress/integration/bdd-cucumber/Reports/SupplyChainReport.feature
@@ -0,0 +1,20 @@
+Feature: DataExporter
+
+
+
+
+    Scenario: Run sc report  with default selection for aggregrated view and verify it runs successfully
+    Given the application is open
+    When I navigate to the Supply chain performance report 
+    And I run the sc report with default selection for aggregrated view
+    Then the sc report should run successfully for aggregrated view
+    
+
+
+
+    Scenario: Run sc report  with default selection for trend view and verify it runs successfully
+    Given the application is open
+    When I navigate to the Supply chain performance report
+    And I run the sc report with default selection for trend view
+    Then the report should run successfully for trend view
+      
